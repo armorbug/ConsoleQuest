09ddbd1d60d9f3ec2a32e233604378b645ed83b6
armorbug <vvladi30@gmail.com>
2024-12-06 18:13:24 +0200

    Level difficulty Room arrangement based on level difficulty Rooms as dictionary

--- Level/Level.cs
+++ Level/Level.cs
@@ -1,5 +1,8 @@
 ﻿//level generation
 
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Levels;
 
 public class Level
@@ -7,41 +10,74 @@ public class Level
     private char[,] currentLevel;
 
     // E - enemy room
-    // N - empty (none) room
-    // H - HP room
-    // W - weapon room  
-    // S - spawn room
-    // X - exit
-    // O - entrance
-    public Level(int depth, int x = 5, int y = 5)
+    // . - empty (none) room
+    // H - HP room - four/two/zero per level, scales with difficulty
+    // W - weapon room - two per level 
+    // S - spawn room - one per level 
+    // X - exit - one per level, guarded by a boss monster 
+    // O - entrance - one per level
+    public Level(int depth, int x = 5, int y = 5, string difficulty = "normal")
     {
         Random random = new Random();
-        char[] rooms = { 'E', 'N', 'H', 'W', 'S' };
-        char[] requiredRooms = {'O','X'};
-
         this.currentLevel = new char[x, y];
+        Dictionary<Char, Int32> rooms = new Dictionary<Char, Int32>();
+        
+        int nonCrucialRooms = x*y - 3; //rooms that are not start, exit, or spawn
 
-        int entranceX = random.Next(x);
-        int entranceY = random.Next(y);
-        int exitX = random.Next(x);
-        int exitY = random.Next(y);
+        // set room count based on difficulty
+        // easy - 4 health rooms, 2 weapon rooms, 1/3 of the rooms have enemies
+        // normal - 3 health rooms, 1 weapon room, 1/2 of the rooms have enemies
+        // hard - 1 health room, 1 weapon room, 2/3 of the rooms have enemies
+        // insane - 0 health rooms, 1 weapon room, 4/5 of the rooms have enemies
+        // crazy - 0 health rooms, 0 weapon rooms, all remaining rooms have enemies 
 
-        currentLevel[entranceX, entranceY] = 'O';
-        currentLevel[exitX, exitY] = 'X';
-
-        for (int i = 0; i < x; i++)
+        switch (difficulty)
         {
-            for (int j = 0; j < y; j++)
+            case "easy":
+                rooms['H'] = 4;
+                rooms['W'] = 2;
+                rooms['E'] = ((nonCrucialRooms) - rooms['H'] - rooms['W'])*(1/3);
+                rooms['.'] = (nonCrucialRooms) - rooms['H'] - rooms['W'] - rooms['E'];
+                break;
+            case "normal":
+                rooms['H'] = 3;
+                rooms['W'] = 1;
+                rooms['E'] = ((nonCrucialRooms) - rooms['H'] - rooms['W'])*(1/2);
+                rooms['.'] = rooms['E'];
+                break;
+            case "hard":
+                rooms['H'] = 1;
+                rooms['W'] = 1;
+                rooms['E'] = ((nonCrucialRooms) - rooms['H'] - rooms['W'])*(2/3);
+                rooms['.'] = (nonCrucialRooms) - rooms['H'] - rooms['W'] - rooms['E'];
+                break;
+            case "insane":
+                rooms['H'] = 0;
+                rooms['W'] = 1;
+                rooms['E'] = ((nonCrucialRooms) - rooms['H'] - rooms['W'])*(4/5);
+                rooms['.'] = (nonCrucialRooms) - rooms['H'] - rooms['W'] - rooms['E'];
+                break;
+            case "crazy":
+                rooms['H'] = 0;
+                rooms['W'] = 0;
+                rooms['E'] = (nonCrucialRooms) - rooms['H'] - rooms['W'];
+                rooms['.'] = 0;
+                break;
+        }
+
+        char[] chars = new char[x*y];
+        int count = 0;
+
+        foreach (char c in rooms.Keys)
+        {
+            for (int i = 0; i < rooms[c]; i++)
             {
-                if (!requiredRooms.Contains(currentLevel[i, j]))
-                {
-                    currentLevel[i, j] = rooms[random.Next(rooms.Length)];
-                }
+                chars[count] = c;
+                count++;
             }
         }
 
-
-
+        Console.WriteLine(chars);
 
     }
 
